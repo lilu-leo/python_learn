146b683da364a56037a16de1b4bb39a8f5254c60
lilu-leo <li854425934@163.com>
2016-11-23 11:13:35 +0800

    add sleep 10s

--- take_a_break.py
+++ take_a_break.py
@@ -1,2 +1,5 @@
+import time
 import webbrowser
+
+time.sleep(10)
 webbrowser.open("http://v.youku.com/v_show/id_XMTgzMDM2OTMyMA==.html?spm=a2hww.20023042.m_223465.5~5~5~5!3~5!2~5~A&from=y1.3-idx-beta-1519-23042.223465.8-1")
